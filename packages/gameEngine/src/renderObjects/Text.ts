4dd40fd74057e462ad77b989488db36070cf5665
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-12-07 15:50:55 +0100

    `Text`: add `maxWidth` property

--- packages/gameEngine/src/renderObjects/Text.ts
+++ packages/gameEngine/src/renderObjects/Text.ts
@@ -13,6 +13,7 @@ import { OptionalArray } from "@typings/utils/OptionalArray"
 
 export interface TextOpts extends StandardOptions, Colorable {
 	text: string | number
+	maxWidth?: number
 	fontFamily?: string
 	fontSize?: number
 	fontSizeUnit?: string
@@ -21,6 +22,7 @@ export interface TextOpts extends StandardOptions, Colorable {
 export class Text extends RenderObject implements RenderObjectType {
 	public type = "text" as const
 	public text
+	public maxWidth
 	public fontFamily
 	public fontSize
 	public fontSizeUnit
@@ -49,11 +51,20 @@ export class Text extends RenderObject implements RenderObjectType {
 		ctx.textAlign = "left"
 		ctx.textBaseline = "top"
 		ctx.fillStyle = this.color
+		ctx.strokeStyle = this.color
 	}
 
 	public getWidth(): number {
 		this.setTextSettings(this.tempCtx)
-		return this.tempCtx.measureText(this.text.toString()).width
+
+		// The width is the lowest number of the maxWidth and the render width
+		const params = [
+			this.tempCtx.measureText(this.text.toString()).width,
+			this.maxWidth,
+		].filter((i): i is number => i !== undefined)
+
+		// The width should be at least 0
+		return Math.max(Math.min(...params), 0)
 	}
 
 	public getHeight(): number {
@@ -63,7 +74,7 @@ export class Text extends RenderObject implements RenderObjectType {
 	public render(ctx: CanvasRenderingContext2D, camera: Camera): void {
 		this.prepareRender(ctx, camera, () => {
 			this.setTextSettings(ctx)
-			ctx.fillText(this.text.toString(), 0, 0)
+			ctx.fillText(this.text.toString(), 0, 0, this.maxWidth)
 		})
 	}
 
@@ -94,6 +105,7 @@ export class Text extends RenderObject implements RenderObjectType {
 		super(opts)
 
 		this.text = opts.text
+		this.maxWidth = opts.maxWidth
 		this.color = opts.color ?? "white"
 		this.fontFamily = opts.fontFamily ?? "sans-serif"
 		this.fontSize = opts.fontSize ?? 10
